70a2bbd4c20a009115b3bd6417b1f22c906f3529
Savita Dhembre <dhembresavita58@gmail.com>
2026-04-17 15:20:21 +0530

    Add files via upload

---
+++ KeepCodingOtherwiseCOFFe.java
@@ -0,0 +1,18 @@
+package com.life;
+
+public class KeepCodingOtherwiseCOFFe {
+	
+	public static void main(String[] args) {
+		
+		String brain="";
+		
+		if(brain != null&&  ! brain.isEmpty()) {
+			System.out.println("keepCoding();");
+		}
+		else {
+			System.out.println("Order COFFee.......");
+		}
+		
+	}
+
+}
